fce3159f4ba1e6581dcf10bc0e9bf4b3d0378e6d
alexandresebrao <alexandre.sebrao@gmail.com>
2015-08-15 08:15:59 -0300

    Ok

--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -130,12 +130,7 @@ STATICFILES_DIRS = (
     STATIC_PATH,
 )
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    TEMPLATE_PATH,
-)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
